4d636c5f354ac2f3a2ba3b8584a55c7bd5936126
Sasaki1994m <wkie2882@yahoo.co.jp>
2020-12-24 18:39:33 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-app>
+        <v-toolbar
+          dark
+          src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+          max-height="60"
+        >
+          <v-toolbar-title>YouTube Curation</v-toolbar-title>
+        </v-toolbar>
+          <top />
+        <v-toolbar
+          dark
+          src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+        >
+          <v-spacer />
+          <p>@QuestAcademia</p>
+        </v-toolbar>
+    </v-app>
+</template>
+
+<script>
+// inport 呼び出し
+  import Top from './components/Top.vue'
+// 出力
+  export default {
+    name: 'App',
+    components: {
+      Top,
+    }
+  }
+</script>
